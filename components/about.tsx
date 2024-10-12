9a350ea30e826d1b2e0a58bccc5d4d3bbec19377
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2024-10-12 17:31:45 +0530

    revert - copy writing

--- components/about.tsx
+++ components/about.tsx
@@ -3,7 +3,7 @@ import LocalTime from "./local-time";
 
 export default function About() {
   return (
-    <div className="lowercase">
+    <>
       <div className="mb-8 md:mb-10 font-mono font-semibold">
         <p>dhruv bhavsar</p>
         <p>
@@ -11,42 +11,32 @@ export default function About() {
         </p>
       </div>
       <div className="space-y-4">
+        <p>i am developer, tinkerer and an ml enthusiast.</p>
         <p>
-          I am a man who builds things with code. My hands know the keyboard
-          like an old friend. Machine learning is my mistress, always calling me
-          back.
-        </p>
-        <p>
-          In the days at{" "}
-          <a
-            href="https://picostone.com"
-            target="_blank"
-            className="underline underline-offset-2 hover:bg-black hover:text-white"
-          >
+          previously, i led the development of products, helped with design and
+          managed cloud resources at{" "}
+          <a href="https://picostone.com" target="_blank" className="underline underline-offset-2 hover:bg-black hover:text-white">
             picostone
           </a>{" "}
-          , I led the charge. We crafted products from nothing but ideas and
-          will. I shaped designs like a sculptor shapes clay. The cloud was my
-          domain, vast and ethereal.
+          as a software developer.
         </p>
         <p>
-          Before that, I walked the halls of{" "}
+          before that, i was a student at{" "}
           <a
             href="https://kjsit.somaiya.edu.in/en"
             target="_blank"
             className="underline underline-offset-2 hover:bg-black hover:text-white"
           >
             kj somaiya
-          </a>
-          (sion). They said I studied AI and data science. The diploma says I
-          learned, but my mind is blank as a fresh sheet of paper.
+          </a>{" "}
+          (sion) and studied bachelor's in ai and data science.
         </p>
+        <p>i don't remember learning anything :)</p>
         <p>
-          Now, each day is a battle against boredom. I create. I build. The
-          things I make light a fire in my belly. Machine learning is my trail
-          through the wilderness. Books are my companions in the quiet hours.
+          i spend my days making things that excite me, learning about ml and
+          reading books.
         </p>
       </div>
-    </div>
+    </>
   );
 }
